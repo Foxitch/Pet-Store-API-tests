ada0294b0faf724675c2ca5d6c69c89d3b897910
maksim.lazko <m.lazko@naveksoft.com>
2023-02-20 17:20:15 +0300

    finished modules/user.py file

--- debug.py
+++
@@ -1,6 +0,0 @@
-a = [{"id":1010,"category":{"id":0,"name":"sheep"},"name":"lamb","photoUrls":["kid"],"tags":[{"id":0,"name":"string"}],"status":"pending"},{"id":760,"category":{"id":0,"name":"<categoryname>"},"name":"man","photoUrls":["woman"],"tags":[{"id":0,"name":"<tag>"}],"status":"pending"}]
-
-
-
-for obj in a:
-    print(obj['status'])
