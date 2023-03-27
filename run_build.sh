83d8ad2745a94862515ea77ae6f3e8315beccf45
maksim.lazko <m.lazko@naveksoft.com>
2023-03-27 22:48:08 +0300

    added docker compose file

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t api-tests .
-docker run api-tests
-docker cp $(docker ps -a -q | head -1):/api_tests/allure-result .
+docker build -t api-tests -f docker/dockerfile .
+docker compose -f docker/docker-compose.yml up
+docker cp "$(docker ps -a -q | head -1)":/opt/api_tests/allure-result .
 allure serve allure-result
