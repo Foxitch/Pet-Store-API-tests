387d9b92d3c3ee37d63eba1612cf181bb3d1ba8a
maksim.lazko <m.lazko@naveksoft.com>
2023-12-06 17:42:00 +0300

    changed file name in the sh path

--- run_build.sh
+++ run_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t api-tests -f docker/dockerfile .
+docker build -t api-tests -f docker/Dockerfile .
 docker compose -f docker/docker-compose.yml up
 docker cp "$(docker ps -a -q | head -1)":/opt/api_tests/allure-result .
 allure serve allure-result
